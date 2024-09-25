2b8a60f505f4f345fdb1794c5d70d80d8cd6d038
Nonagono <chasedbb10@icloud.com>
2024-09-25 11:03:10 -0600

    Started  creating board for interactive scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,11 +1,13 @@
-// Interactive Scene: (Placeholder)
+// Interactive Scene: Connect 4
 // Chase Buniak
 // Oct 1, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+const GAME_BOARD_WIDTH = 900;
+const GAME_BOARD_HEIGHT = 880;
+const CIRCLE_RADIUS = 100;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,4 +15,18 @@ function setup() {
 
 function draw() {
   background(220);
+  gameBaord();
+}
+
+function gameBaord() {
+  fill("blue");
+  rect(0, 0, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
+}
+
+function holes() {
+  for (let x = CIRCLE_RADIUS; x <= GAME_BOARD_WIDTH - CIRCLE_RADIUS; x++) {
+    for (let y = CIRCLE_RADIUS; x <= GAME_BOARD_HEIGHT - CIRCLE_RADIUS; y++) {
+      //placeholder
+    }
+  }
 }
