b6f22a5fa71c0e4a86ec23eaa587184d29dea071
Buniak3201072 <3201072@spsd.sk.ca>
2024-09-27 11:02:16 -0600

    Made some bad code
    Made something to know where you would be placing a circle, then realized it was bad and dumb and deleted it for a better option that isn't working yet

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -8,6 +8,10 @@
 const GAME_BOARD_WIDTH = 900;
 const GAME_BOARD_HEIGHT = 880;
 const CIRCLE_RADIUS = 40;
+let arrow1 = 90;
+let arrow2 = 120;
+let arrow3 = 150;
+let checkPixel;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,18 +24,39 @@ function draw() {
 
 function gameBoard() {
   fill("blue");
-  for (let x = 0; x < GAME_BOARD_WIDTH; x += GAME_BOARD_WIDTH/7) {
-    // fill(random(255), random(255), random(255));
-    rect(0, 0, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT)/7;
-  }
+  rect(0, 0, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
   holes();
 }
 
 function holes() {
-  for (let x = 90; x < GAME_BOARD_WIDTH; x += 120) {
-    for (let y = 90; y <  GAME_BOARD_HEIGHT; y+= 120) {
+  for (let y = 90; y <  GAME_BOARD_HEIGHT; y+= 120) {
+    for (let x = 90; x < GAME_BOARD_WIDTH; x += 120) {
       fill("white");
       circle(x, y, CIRCLE_RADIUS * 2);
     }
+  }
+}
+
+function placementArrow() {
+  triangle(arrow1, 30, arrow2, 60, arrow3, 30);
+}
+
+
+// function chips() {
+//   if (keyIsPressed(32)) {
+//     checkPixel()
+//   }
+// }
+
+function mouseWheel(event) {
+  if (event.delta < 0) {
+    arrow1 += 120;
+    arrow2 += 120;
+    arrow3 += 120;
+  }
+  else if (event.delta > 0) {
+    arrow1 -= 120;
+    arrow2 -= 120;
+    arrow3 -= 120;
   }
 }
