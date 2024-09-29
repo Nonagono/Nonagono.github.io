1fc5637a2e0c5725d1c2cbf8cdc394e7e16185d2
Nonagono <chasedbb10@icloud.com>
2024-09-28 23:15:02 -0600

    Fixed scrolling, added array, added player turns
    Added an array that allows for checking if a circle is white or not, also added the function of changing circle colours and player turns

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,15 +3,29 @@
 // Oct 1, 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// - Used an array for circle positions, used the scroll wheel function to have a triangle move around.
 
+
+// Constants and variables used
 const GAME_BOARD_WIDTH = 900;
 const GAME_BOARD_HEIGHT = 880;
 const CIRCLE_RADIUS = 40;
-let arrow1 = 90;
-let arrow2 = 120;
-let arrow3 = 150;
+const CIRCLE_DEVIATION = 120;
+const BOARD_OFFSET = 90;
+let arrowX1 = 60;
+let arrowX2 = 90;
+let arrowX3 = 120;
 let checkPixel;
+let columnPosition = 0;
+let rowPosition = 0;
+let boardArray = [[0,0,0,0,0,0,0], 
+                  [0,0,0,0,0,0,0], 
+                  [0,0,0,0,0,0,0], 
+                  [0,0,0,0,0,0,0], 
+                  [0,0,0,0,0,0,0], 
+                  [0,0,0,0,0,0,0]]
+let playerTurn = 1;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -20,43 +34,78 @@ function setup() {
 function draw() {
   background(220);
   gameBoard();
+  chips();
 }
 
+//Creates the background for the game board
 function gameBoard() {
   fill("blue");
   rect(0, 0, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
   holes();
+  placementArrow();
 }
 
+//Creates all of the circles in the board; coloured white.
 function holes() {
-  for (let y = 90; y <  GAME_BOARD_HEIGHT; y+= 120) {
-    for (let x = 90; x < GAME_BOARD_WIDTH; x += 120) {
+  for (let x = BOARD_OFFSET; x < GAME_BOARD_WIDTH; x += CIRCLE_DEVIATION) {
+    for (let y = BOARD_OFFSET; y < GAME_BOARD_HEIGHT; y+= CIRCLE_DEVIATION) {
       fill("white");
       circle(x, y, CIRCLE_RADIUS * 2);
     }
   }
 }
 
+// Creates a triangle to show which column your chip will be placed in.
 function placementArrow() {
-  triangle(arrow1, 30, arrow2, 60, arrow3, 30);
+  triangle(arrowX1, 20, arrowX2, 50, arrowX3, 20);
 }
 
-
-// function chips() {
-//   if (keyIsPressed(32)) {
-//     checkPixel()
-//   }
-// }
-
+//Moves the position of placement arrow using the scroll wheel. Scrolling towards yourself makes it go left, scrolling away from yourself makes it go right.
 function mouseWheel(event) {
   if (event.delta < 0) {
-    arrow1 += 120;
-    arrow2 += 120;
-    arrow3 += 120;
+    if (arrowX3 < GAME_BOARD_WIDTH - CIRCLE_DEVIATION) {
+      arrowX1 += CIRCLE_DEVIATION;
+      arrowX2 += CIRCLE_DEVIATION;
+      arrowX3 += CIRCLE_DEVIATION;
+    }
   }
   else if (event.delta > 0) {
-    arrow1 -= 120;
-    arrow2 -= 120;
-    arrow3 -= 120;
+    if (arrowX1 > CIRCLE_DEVIATION) {
+      arrowX1 -= CIRCLE_DEVIATION;
+      arrowX2 -= CIRCLE_DEVIATION;
+      arrowX3 -= CIRCLE_DEVIATION;
+    }
+  }
+}
+
+// Creates a coloured circle based on whose turn it is, checking to see where to put it using an array, then changes the player turn state to allow the next player to place a chip.
+function chips() {
+  if (keyIsPressed) {
+    if (keyCode === 32){
+      if (playerTurn === 1) {
+        columnPosition = ((arrowX2 - BOARD_OFFSET)/120) - 1;
+        for (let i = 0; i < 6; i++) {
+          if (boardArray[columnPosition][i] === 0) {
+            rowPosition = i;
+          }
+        }
+        fill("red");
+        circle((columnPosition + 1) * 120 + BOARD_OFFSET, (rowPosition + 1) * 120 + BOARD_OFFSET, CIRCLE_RADIUS * 2);
+        boardArray[columnPosition][rowPosition] = 1;
+        playerTurn = 2;
+      }
+      if (playerTurn === 2) {
+        columnPosition = ((arrowX2 - BOARD_OFFSET)/120) - 1;
+        for (let i = 0; i < 6; i++) {
+          if (boardArray[columnPosition][i] === 0) {
+            rowPosition = i;
+          }
+        }
+        fill("yellow");
+        circle((columnPosition + 1) * 120 + BOARD_OFFSET, (rowPosition + 1) * 120 + BOARD_OFFSET, CIRCLE_RADIUS * 2);
+        boardArray[columnPosition][rowPosition] = 2;
+        playerTurn = 1;
+      }
+    }
   }
 }
