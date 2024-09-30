27dd2bae87f212b955eb39af4dbde0e30a8d81d6
Nonagono <chasedbb10@icloud.com>
2024-09-29 22:35:43 -0600

    coded for 8 hours
    coded for 8 hours with some small breaks, made so many changes that I have no idea what to put here

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -3,7 +3,7 @@
 // Oct 1, 2024
 //
 // Extra for Experts:
-// - Used an array for circle positions, used the scroll wheel function to have a triangle move around.
+// - Used an array/matrix for circle positions, used the scroll wheel function to have a triangle move around.
 
 
 // Constants and variables used
@@ -15,18 +15,14 @@ const BOARD_OFFSET = 90;
 let arrowX1 = 60;
 let arrowX2 = 90;
 let arrowX3 = 120;
-let checkPixel;
 let columnPosition = 0;
-let rowPosition = 0;
-let boardArray = [[0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0], 
-                  [0,0,0,0,0,0,0]]
+let rows = 6;
+let columns = 7;
+//let boardArray = Array(6).fill().map(() => Array(7).fill(1));
+let boardArray = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
 let playerTurn = 1;
-
+let i = 5;
+let arrayValue;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -34,11 +30,11 @@ function setup() {
 
 function draw() {
   background(220);
-  chips();
   gameBoard();
+  colourCircles();
 }
 
-//Creates the background for the game board
+//Creates the background for the game board and runs the holes and placementArrow functions.
 function gameBoard() {
   fill("blue");
   rect(0, 0, GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
@@ -46,12 +42,18 @@ function gameBoard() {
   placementArrow();
 }
 
-//Creates all of the circles in the board; coloured white.
+//Creates all of the circles in the board; coloured white until the state of a number in the array changes. If the number is 1 then the circle will be red if the number is 2 then the circle will be yellow.
 function holes() {
-  for (let x = BOARD_OFFSET; x < GAME_BOARD_WIDTH; x += CIRCLE_DEVIATION) {
-    for (let y = BOARD_OFFSET; y < GAME_BOARD_HEIGHT; y+= CIRCLE_DEVIATION) {
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
       fill("white");
-      circle(x, y, CIRCLE_RADIUS * 2);
+      if (boardArray[r][c] === 2) {
+        fill("red");
+      }
+      if (boardArray[r][c] === 3) {
+        fill("yellow");
+      }
+    circle(c * CIRCLE_DEVIATION + BOARD_OFFSET, r * CIRCLE_DEVIATION + BOARD_OFFSET, CIRCLE_RADIUS * 2);
     }
   }
 }
@@ -68,6 +70,7 @@ function mouseWheel(event) {
       arrowX1 += CIRCLE_DEVIATION;
       arrowX2 += CIRCLE_DEVIATION;
       arrowX3 += CIRCLE_DEVIATION;
+      columnPosition += 1;
     }
   }
   else if (event.delta > 0) {
@@ -75,37 +78,46 @@ function mouseWheel(event) {
       arrowX1 -= CIRCLE_DEVIATION;
       arrowX2 -= CIRCLE_DEVIATION;
       arrowX3 -= CIRCLE_DEVIATION;
+      columnPosition -= 1;
     }
   }
 }
 
-// Creates a coloured circle based on whose turn it is, checking to see where to put it using an array, then changes the player turn state to allow the next player to place a chip.
-function chips() {
-  if (keyIsPressed) {
-    if (keyCode === 32){
-      if (playerTurn === 1) {
-        columnPosition = ((arrowX2 - BOARD_OFFSET)/120) - 1;
-        for (let i = 0; i < 6; i++) {
-          if (boardArray[columnPosition][i] === 0) {
-            rowPosition = i;
+// When the space bar is pressed, changes the colour of the lowest circle it can
+function colourCircles() {
+  if (keyIsDown(32)) {
+    if (playerTurn === 1) {
+      while(playerTurn === 1) {
+        arrayValue = boardArray[i][columnPosition];
+        console.log(arrayValue);
+        if (arrayValue === 0) {
+          boardArray[i][columnPosition] = 2;
+          for (let timer = 0; timer < 100000; timer++) {
+            console.log(timer);
           }
+          i = 5;
+          playerTurn = 2;
         }
-        fill("red");
-        circle((columnPosition + 1) * 120 + BOARD_OFFSET, (rowPosition + 1) * 120 + BOARD_OFFSET, CIRCLE_RADIUS * 2);
-        boardArray[columnPosition][rowPosition] = 1;
-        playerTurn = 2;
+        else if(i > 0){
+          i -= 1;
+        }
       }
-      if (playerTurn === 2) {
-        columnPosition = ((arrowX2 - BOARD_OFFSET)/120) - 1;
-        for (let i = 0; i < 6; i++) {
-          if (boardArray[columnPosition][i] === 0) {
-            rowPosition = i;
+    }
+    if (playerTurn === 2) {
+      while(playerTurn === 2) {
+        arrayValue = boardArray[i][columnPosition];
+        console.log(arrayValue);
+        if (arrayValue === 0) {
+          boardArray[i][columnPosition] = 3;
+          for (let timer = 0; timer < 100000; timer++) {
+            console.log(timer);
           }
+          i = 5;
+          playerTurn = 1;
         }
-        fill("yellow");
-        circle((columnPosition + 1) * 120 + BOARD_OFFSET, (rowPosition + 1) * 120 + BOARD_OFFSET, CIRCLE_RADIUS * 2);
-        boardArray[columnPosition][rowPosition] = 2;
-        playerTurn = 1;
+        else if(i > 0){
+          i -= 1;
+        }
       }
     }
   }
