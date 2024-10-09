c24d636dd36132445d82ca5f21a38841cd1158d3
Nonagono <chasedbb10@icloud.com>
2024-10-09 11:03:31 -0600

    Started with some formatting, created an object
    Decided on a project idea, made an object that will be needed, started some formatting with comments.

--- array-objects/sketch.js
+++ array-objects/sketch.js
@@ -1,4 +1,4 @@
-// Arrays and Object Notation: Placeholder
+// Arrays and Object Notation: Chrome T-Rex Game
 // Chase Buniak
 // October 21, 2024
 //
@@ -6,10 +6,22 @@
 // - describe what you did to take this project "above and beyond"
 
 
+// Variables and constants used
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+}
+
+
+// Holds the variables for the object Cactus
+function spawnCactus() {
+  let cactus = {
+    dx: 1,
+    catusHeight: random(5, 10),
+    catusWidth: 5,
+  };
 }
