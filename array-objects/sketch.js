f2f3100058ab788a95ced09cc2050f99e3497583
Nonagono <chasedbb10@icloud.com>
2024-10-21 22:14:57 -0600

    Finished game.
    Completed gravity function, added collision and game over, changed from cacti to boulders, added boulder movement, added a function to let boulders disappear, added an interval to spawn boulders, cleaned up code.

--- array-objects/sketch.js
+++ array-objects/sketch.js
@@ -3,27 +3,28 @@
 // October 21, 2024
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// Used audio features by importing an mp3 and using the play and volume functions.
 
 
-// Variables and constants used
-let cacti = [];
+// Variables used
+let boulders = [];
 let bGCState = true;
 let bGC = "white";
 let y;
+let dy = 0;
+let jumpPower = 25;
 let ground;
 let characterPosition;
-let characterHeight = 100;
-let yx = 0;
-let start = false;
+let characterDiameter = 100;
+let hide = false;
 let universalGrey = 255/2;
 let song;
 let musicPlaying = false;
 let v = 0.5;
-let g = 0.1;
-let drag;
-let isJumping = false;
-
+let g = 1.5;
+let airPosition;
+const MINIMUM_DISTANCE_POSSIBLE = 90;
+let intervalID;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -31,43 +32,74 @@ function setup() {
   y = ground;
   characterPosition = width/8;
   song = createAudio('tetris-theme.mp3');
+
+  // Creates a boulder every three quarters of a second.
+  intervalID = window.setInterval(createBoulder, 750)
 }
 
 function draw() {
   background(bGC);
   displayGame();
+  spawnBoulder();
+  moveBoulders();
+  byeBoulders();
+  playerGravity();
   crouch();
   playSong();
+  gameOver();
 }
 
-// Displays every element of the game
+// Displays every visible element of the game.
 function displayGame() {
   noStroke();
   displayButton();
   displayCharacter();
   displayGround();
-  displayCacti();
+  displayBoulder();
 }
 
-// Displays a grey button at the top right of the screen
+// Displays a grey button with text at the top right of the screen.
 function displayButton() {
   fill(universalGrey);
   rect(width - 75, 0, 75, 75); 
+  if (bGCState) {
+    fill(bGC);
+    text("Dark", width - 70, 45);
+  }
+  else if (!bGCState) {
+    fill(bGC);
+    text("Light", width - 70, 45);
+  }
 }
 
-// Creates a pink ellipse with blue eyes to act as the player character 
+// Creates a pink ellipse with blue eyes to act as the player character.
 function displayCharacter() {
   fill('pink');
-  ellipse(characterPosition, y - characterHeight/2, 100, characterHeight);
+  circle(characterPosition, y - characterDiameter/2, characterDiameter);
   fill('mediumslateblue');
-  circle(characterPosition, y - characterHeight/1.5, 10);
-  circle(characterPosition + 20, y - characterHeight/1.5, 10);
+  circle(characterPosition, y - characterDiameter/1.5, 10);
+  circle(characterPosition + 20, y - characterDiameter/1.5, 10);
 }
 
-// Creates a line to act as the ground for the character and cacti
+// Creates a line to act as the ground for the character and boulders.
 function displayGround() {
   stroke(universalGrey);
-  line(0, height/3 * 2, width, height/3 * 2);
+  line(0, ground, width, ground);
+}
+
+// Creates circles to act as boulders and flying boulders.
+function displayBoulder() {
+  for (let boulder of boulders) {
+    fill(universalGrey);
+    circle(boulder.boulderX, boulder.boulderY, boulder.boulderDiameter);
+  }
+}
+
+// Moves the boulders to the left at a constant rate.
+function moveBoulders() {
+  for (let boulder of boulders) {
+    boulder.boulderX -= boulder.dx;
+  }
 }
 
 // Changes the background colour between white and black when you click inside the bounds of displayButton().
@@ -76,74 +108,115 @@ function mousePressed() {
     bGCState = !bGCState;
     if (bGCState) {
       bGC = "white";
+      fill(bGC);
+      text("Dark", width - 70, 75/2);
     }
     else if (!bGCState) {
       bGC = "black";
+      fill(bGC);
+      text("Light", width - 70, 75/2);
     }
   }
 }
 
-// Holds the variables for the object someCactus
-function spawnCactus() {
-  let someCactus = {
-    dx: 1,
-    catusHeight: random(5, 10),
-    catusWidth: 5,
+// Holds the variables for the object someBoulder.
+function createBoulder() {
+  let someBoulder = {
+    dx: 10,
+    boulderY: airBoulder(),
+    boulderX: width,
+    boulderDiameter: 100,
   };
 
-  cacti.push(someCactus);
+  boulders.push(someBoulder);
 }
 
-// Sets the start state to true when the space bar is pressed, runs the jump function when the up arrow is pressed, plays music when m is pressed.
+// Returns a number to spawn a boulder either in the air or on the ground.
+function airBoulder() {
+  let num = random(4);
+  if (num <= 3) {
+    return ground;
+  }
+  else {
+    return (ground - 130);
+  }
+}
+
+// Sets the hide state to true when the space bar is pressed, has the player jump when the up arrow is pressed and the down arrow is not being held, plays music when m is pressed.
 function keyPressed() {
   if (keyCode === 32) {
-    start = true;
+    hide = true;
   }
   if (keyCode === 77) {
     musicPlaying = !musicPlaying;
   }
   if (keyCode === 38) {
-    if (!isJumping) {
-      isJumping = true;
-      jump();
+    if (y === ground && !keyIsDown(DOWN_ARROW)) {
+      dy = -jumpPower;
     }
   }
 }
 
-function jump() {
-  if (isJumping) {
-    yx += g;
-    y -= yx;
-    yx -= 0.01;
-  }
-  isJumping = false;
-}
-
-// Reduces the characterHeight variable to simulate crouching if the down arrow is held
-function crouch() {
-  if (keyIsDown(DOWN_ARROW)) {
-    characterHeight = 75;
+// Gives the player simulated gravity tied to the frame rate rather than computer hardware.
+function playerGravity() {
+  y += dy;
+  if (y <= ground) {
+    dy += g;
   }
   else {
-    characterHeight = 100;
+    y = ground;
+    dy = 0;
   }
 }
 
-// Displays text with the controls until the space bar is pressed , then starts spawning obstacles
-function displayCacti() {
-  if (start === false) {
+// Reduces the characterDiameter variable to simulate crouching if the down arrow is held and the player is on the ground.
+function crouch() {
+  if (keyIsDown(DOWN_ARROW) && y === ground) {
+    characterDiameter = 75;
+  }
+  else {
+    characterDiameter = 100;
+  }
+}
+
+// Displays text with the controls until the space bar is pressed , then has the text disappear
+function spawnBoulder() {
+  if (hide === false) {
     fill(universalGrey);
     textSize(30);
-    text("Up arrow to jump, down arrow to crouch, space to start, m to play the tetris theme.", width/5, height/3);
-  }
-  else {
-    // 
+    text("Up arrow to jump, down arrow to crouch, m to play the tetris theme, space to hide text.", width/5, height/3);
   }
 }
 
+// Makes boulders disappear when they reach the far left side of the screen.
+function byeBoulders() {
+  for (let boulder of boulders) {
+    if (boulder.boulderX === 0) {
+      let index = boulders.indexOf(boulder);
+      boulders.splice(index, 1);
+    }
+  }
+}
+
+// Calculates the distance between the character and the boulders to know if you get hit, stops the spawning of boulders if you are hit and displays game ever text.
+function gameOver() {
+  for (let boulder of boulders) {
+    let d = dist(characterPosition, y - characterDiameter/2, boulder.boulderX, boulder.boulderY)
+    if (d < MINIMUM_DISTANCE_POSSIBLE) {
+      boulder.dx = 0
+      fill(universalGrey);
+      textSize(50);
+      text("Game Over! Refresh to play again.", width/3.5, height/2.5);
+      window.clearInterval(intervalID);
+    }
+  }
+}
+
+// Plays music if the m key has been pressed to set musicPlaying to true.
 function playSong() {
   song.volume(v);
   if (musicPlaying === true) {
     song.play();
+    musicPlaying = false;
   }
 }
--- test/sketch.js
+++ test/sketch.js
@@ -7,116 +7,13 @@
 
 
 // Variables and constants used
-let cacti = [];
-let bGCState = true;
-let bGC = "white";
-let y;
-let characterPosition;
-let characterHeight = 100;
-let yx = 1;
-let start = false;
-let universalGrey = 255/2;
-let song = createAudio('tetris-theme.mp3');
-let v = 1;
+
 
 function setup() {
-  createCanvas(windowWidth, 600);
-  y = height/3 *2;
-  characterPosition = width/8;
+
 }
 
 function draw() {
-  background(bGC);
-  displayGame();
-  crouch();
-}
-
-// Displays every element of the game
-function displayGame() {
-  noStroke();
-  displayButton();
-  displayCharacter();
-  displayGround();
-  displayCacti();
-}
-
-// Displays a grey button at the top right of the screen
-function displayButton() {
-  fill(universalGrey);
-  rect(width - 75, 0, 75, 75); 
-}
-
-// Creates a pink ellipse with blue eyes to act as the player character 
-function displayCharacter() {
-  fill('pink');
-  ellipse(characterPosition, y - characterHeight/2, 100, characterHeight);
-  fill('mediumslateblue');
-  circle(characterPosition, y - characterHeight/1.5, 10)
-  circle(characterPosition + 20, y - characterHeight/1.5, 10)
-}
-
-// Creates a line to act as the ground for the character and cacti
-function displayGround() {
-  stroke(universalGrey);
-  line(0, height/3 * 2, width, height/3 * 2);
-}
-
-// Changes the background colour between white and black when you click inside the bounds of displayButton().
-function mousePressed() {
-  if (mouseX >= width - 75 && mouseY <= 75) {
-    bGCState = !bGCState;
-    if (bGCState) {
-      bGC = "white";
-    }
-    else if (!bGCState) {
-      bGC = "black";
-    }
-  }
-}
-
-// Holds the variables for the object someCactus
-function spawnCactus() {
-  let someCactus = {
-    dx: 1,
-    catusHeight: random(5, 10),
-    catusWidth: 5,
-  };
-
-  cacti.push(someCactus);
-}
-
-// Sets the start state to true when the space bar is pressed, jumps when the up arrow is pressed, plays music when m is pressed.
-function keyPressed() {
-  if (keyCode === 32) {
-    start = true;
-  }
-  if (keyCode === 77) {
-    song.play();
-  }
-}
-
-// Reduces the characterHeight variable to simulate crouching if the down arrow is held
-function crouch() {
-  if (keyIsDown(DOWN_ARROW)) {
-    characterHeight = 75;
-  }
-  else {
-    characterHeight = 100;
-  }
-}
-
-// Displays text with the controls until the space bar is pressed , then starts spawning obstacles
-function displayCacti() {
-  if (start === false) {
-    fill(universalGrey);
-    textSize(30);
-    text("Up arrow to jump, down arrow to crouch, space to start", width/3.5, height/3);
-  }
-  else {
-    // 
-  }
-}
-
-function playMusic() {
-  song.volume(v);
+  airPosition = noise(999999999) * 2;
+  console.log(airPosition)
 }
