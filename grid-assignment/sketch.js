6a8f55d5309957a5682e5e9404d52d335c423404
Nonagono <chasedbb10@icloud.com>
2024-10-30 11:02:30 -0600

    Made the full board display
    Changed the boards color, made the display board function work for every possible case of piece (regular or king for player and bot), left myself some notes on what to make, started on a function that allows the player to know what piece they have selected, got rid of some magic numbers.

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -1,6 +1,6 @@
 // Grid Based Game
 // Chase Buniak
-// November 3, 2024
+// November 8, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
@@ -8,7 +8,14 @@
 
 let grid;
 let cellSize;
-const GRID_SIZE = 20;
+const GRID_SIZE = 8;
+let tileIsLight = true;
+const LIGHT_TILE = 0;
+const DARK_TILE = 1;
+let playerPiece = 3;
+let playerKing = 4;
+let botPiece = 5;
+let botKing = 6;
 
 function setup() {
   if (windowWidth < windowHeight) {
@@ -18,7 +25,7 @@ function setup() {
     createCanvas(windowHeight, windowHeight);
   }
   cellSize = height/GRID_SIZE;
-  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  grid = generateBoard(GRID_SIZE, GRID_SIZE);
 }
 
 function windowResized() {
@@ -36,44 +43,90 @@ function draw() {
   displayGrid();
 }
 
-function keyPressed() {
-  if (key === 'r') {
-    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
-  }
-  if (key === 'e') {
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-}
 function mousePressed() {
-// 
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  if (grid[y][x] === playerPiece || grid[y][x] === playerKing) {
+    // highlightSquare(x, y);
+  }
 }
+
+// function highlightSquare(x, y) {
+// 
+// }
 
 function displayGrid() {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
-      if (grid[y][x] === 1) {
-        fill('black');
+      if (grid[y][x] === DARK_TILE) {
+        makeSquares(x, y);
+        square(x * cellSize, y * cellSize, cellSize);
       }
-      else if (grid[y][x] === 0) {
-        fill('white');
+      else if (grid[y][x] === LIGHT_TILE) {
+        makeSquares(x, y);
       }
-      square(x * cellSize, y * cellSize, cellSize);
+      else if (grid[y][x] === playerPiece) {
+        makeSquares(x, y);
+        fill(20, 20, 20);
+        circle(x * cellSize + cellSize/2, y * cellSize + cellSize/2, cellSize/1.5);
+      }
+      else if (grid[y][x] === playerKing) {
+        makeSquares(x, y);
+        fill(0);
+        circle(x * cellSize + cellSize/2, y * cellSize + cellSize/2, cellSize/1.5);
+      }
+      else if (grid[y][x] === botPiece) {
+        makeSquares(x, y);
+        fill(200, 200, 200);
+        circle(x * cellSize + cellSize/2, y * cellSize + cellSize/2, cellSize/1.5);
+      }
+      else if (grid[y][x] === botKing) {
+        makeSquares(x, y);
+        fill(255);
+        circle(x * cellSize + cellSize/2, y * cellSize + cellSize/2, cellSize/1.5);
+      }
     }
   }
 }
 
-function generateRandomGrid(cols, rows) {
+function makeSquares(x, y) {
+  if (grid[y][x] === DARK_TILE) {
+    fill(85, 52, 43);
+  }
+  else {
+    fill(216, 181, 137);
+  }
+  square(x * cellSize, y * cellSize, cellSize);
+}
+
+function generateBoard(cols, rows) {
   let newGrid = [];
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
+    tileIsLight = !tileIsLight;
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 50) {
-        newGrid[y].push(0);
+      if (tileIsLight && y > 4) {
+        newGrid[y].push(playerPiece);
+      }
+      else if (tileIsLight && y < 3) {
+        newGrid[y].push(botPiece);
+      }
+      else if (tileIsLight) {
+        newGrid[y].push(LIGHT_TILE);
       }
       else {
-        newGrid[y].push(1);
+        newGrid[y].push(DARK_TILE);
       }
+      tileIsLight = !tileIsLight;
     }
   }
   return newGrid;
 }
+
+
+// Create a way to know what piece is selected
+// Make sure when piece is moved tile stays same colour; Likely white
+// Make a way for piece to mave
+// Make a way to capture
+// Create smart Ai (Make a func to check for possible capture)
