9c0d116a03fd0727157858ca6a68b3215730decb
Nonagono <chasedbb10@icloud.com>
2024-10-31 10:45:34 -0600

    Created a secondary experimental version
    Came to the realization that the way I had made my pieces was not going to work so I went to fix it and that will be a big overhaul (I think) so I stuck the experimental version into a different file for now.

--- tests/sketch.js
+++ tests/sketch.js
@@ -1,15 +1,143 @@
-// Project Title
-// Your Name
-// Date
+// Grid Based Game
+// Chase Buniak
+// November 8, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 
+let grid;
+let cellSize;
+const GRID_SIZE = 8;
+let tileIsLight = true;
+const LIGHT_TILE = 0;
+const DARK_TILE = 1;
+let playerPiece = 3;
+let playerKing = 4;
+let botPiece = 5;
+let botKing = 6;
+
 function setup() {
-  createCanvas(200, 200);
+  if (windowWidth < windowHeight) {
+    createCanvas(windowWidth, windowWidth);
+  }
+  else {
+    createCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
+  grid = generateBoard(GRID_SIZE, GRID_SIZE);
+}
+
+function windowResized() {
+  if (windowWidth < windowHeight) {
+    resizeCanvas(windowWidth, windowWidth);
+  }
+  else {
+    resizeCanvas(windowHeight, windowHeight);
+  }
+  cellSize = height/GRID_SIZE;
 }
 
 function draw() {
-
+  background(220);
+  displayGrid();
 }
+
+function mousePressed() {
+  let x = Math.floor(mouseX/cellSize);
+  let y = Math.floor(mouseY/cellSize);
+
+  if (grid[y][x] === playerPiece || grid[y][x] === playerKing) {
+    // highlightSquare(x, y);
+  }
+}
+
+// function highlightSquare(x, y) {
+// 
+// }
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (grid[y][x] === DARK_TILE) {
+        makeSquares(x, y);
+      }
+      else if (grid[y][x] === LIGHT_TILE) {
+        makeSquares(x, y);
+      }
+      else if (grid[y][x] === playerPiece) {
+        makeSquares(x, y);
+        fill(20, 20, 20);
+        makeCircle(x, y);
+      }
+      else if (grid[y][x] === playerKing) {
+        makeSquares(x, y);
+        fill(0);
+        makeCircle(x, y);
+      }
+      else if (grid[y][x] === botPiece) {
+        makeSquares(x, y);
+        fill(200, 200, 200);
+        makeCircle(x, y);
+      }
+      else if (grid[y][x] === botKing) {
+        makeSquares(x, y);
+        fill(255);
+        makeCircle(x, y);
+      }
+    }
+  }
+}
+
+function makeSquares(x, y) {
+  if (grid[y][x] === DARK_TILE) {
+    fill(85, 52, 43);
+  }
+  else {
+    fill(216, 181, 137);
+  }
+  square(x * cellSize, y * cellSize, cellSize);
+}
+
+function makePieces(x, y) {
+  // let basicPiece = {
+  //   pieceX: x * cellSize + cellSize/2,
+  //   pieceY: y * cellSize + cellSize/2,
+  //   pieceD: cellSize/1.5,
+  //   pieceStroke: 0,
+  // };
+
+  // .push(basicPiece)
+  circle(basicPiece.pieceX, basicPiece.pieceY, basicPiece.pieceD);
+}
+
+function generateBoard(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    tileIsLight = !tileIsLight;
+    for (let x = 0; x < cols; x++) {
+      if (tileIsLight && y > 4) {
+        newGrid[y].push(playerPiece);
+      }
+      else if (tileIsLight && y < 3) {
+        newGrid[y].push(botPiece);
+      }
+      else if (tileIsLight) {
+        newGrid[y].push(LIGHT_TILE);
+      }
+      else {
+        newGrid[y].push(DARK_TILE);
+      }
+      tileIsLight = !tileIsLight;
+    }
+  }
+  return newGrid;
+}
+
+
+// Create a way to know what piece is selected
+// Make sure when piece is moved tile stays same colour; Likely white
+// Make a way for piece to mave
+// Make a way to capture
+// Create smart Ai (Make a func to check for possible capture)
