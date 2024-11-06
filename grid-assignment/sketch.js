fe34d779f6050c1d65c034cb344aa0c2a05489a7
Nonagono <chasedbb10@icloud.com>
2024-11-06 11:02:23 -0600

    Reverted back to previous version, started on movement
    The version of code I had was not working for me and I wasn't too sure where to go with it, so I went back to a previous version I had saved just in case this exact thing happened. Started working on movement within this version of code.

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -7,19 +7,15 @@
 
 
 let grid;
-let pieces;
 let cellSize;
 const GRID_SIZE = 8;
 let tileIsLight = true;
-let shouldBePiece = true;
 const LIGHT_TILE = 0;
 const DARK_TILE = 1;
 let playerPiece = 2;
 let botPiece = 3;
 let selected = 0;
-let selectedY;
-let selectedX;
-
+    
 function setup() {
   if (windowWidth < windowHeight) {
     createCanvas(windowWidth, windowWidth);
@@ -28,10 +24,9 @@ function setup() {
     createCanvas(windowHeight, windowHeight);
   }
   cellSize = height/GRID_SIZE;
-  grid = generateBoardArray(GRID_SIZE, GRID_SIZE);
-  pieces = generatePiecesArray(GRID_SIZE, GRID_SIZE);
+  grid = generateBoard(GRID_SIZE, GRID_SIZE);
 }
-
+    
 function windowResized() {
   if (windowWidth < windowHeight) {
     resizeCanvas(windowWidth, windowWidth);
@@ -41,27 +36,28 @@ function windowResized() {
   }
   cellSize = height/GRID_SIZE;
 }
-
+    
 function draw() {
   background(220);
-  displayBoard();
-  displayPieces();
+  displayGrid();
 }
-
+    
 function mousePressed() {
   let clickX = Math.floor(mouseX/cellSize);
   let clickY = Math.floor(mouseY/cellSize);
-
+    
   for (let piece of grid) {
     if (grid[clickY][clickX] === playerPiece) {
-      selectedY = clickY;
-      selectedX = clickX;
+      piece.pieceStroke = 255;
+    }
+    else {
+      piece.pieceStroke = 0;
     }
   }
 }
 
-
-function displayBoard() {
+    
+function displayGrid() {
   for (let y = 0; y < GRID_SIZE; y++) {
     for (let x = 0; x < GRID_SIZE; x++) {
       stroke(0);
@@ -71,10 +67,20 @@ function displayBoard() {
       else if (grid[y][x] === LIGHT_TILE) {
         makeSquares(x, y);
       }
+      else if (grid[y][x] === playerPiece) {
+        makeSquares(x, y);
+        fill(20, 20, 20);
+        makePieces(x, y);
+      }
+      else if (grid[y][x] === botPiece) {
+        makeSquares(x, y);
+        fill(200, 200, 200);
+        makePieces(x, y);
+      }
     }
   }
 }
-
+    
 function makeSquares(x, y) {
   if (grid[y][x] === DARK_TILE) {
     fill(85, 52, 43);
@@ -84,89 +90,53 @@ function makeSquares(x, y) {
   }
   square(x * cellSize, y * cellSize, cellSize);
 }
-
-function displayPieces() {
-  for (let y = 0; y < GRID_SIZE; y++) {
-    for (let x = 0; x < GRID_SIZE; x++) {
-      if (pieces[y][x] === 2) {
-        piecesObject(x, y);
-        makePieces(x, y);
-      }
-      else if (pieces[y][x] === 3) {
-        piecesObject(x, y);
-        makePieces(x, y);
-      }
-    }
-  }
-}
-
+    
 function makePieces(x, y) {
-  for (let square of pieces) {
-    fill(square.colour);
-    circle(square.pieceX, square.pieceY, square.pieceD);
-  }
-}
-
-function piecesObject(x, y) {
   let pieceGoalFinder;
-  let colorPlaceholder;
   if (y > 4) {
     pieceGoalFinder = 8;
-    colorPlaceholder = 'blue';
   }
   else if (y < 3) {
     pieceGoalFinder = 0;
-    colorPlaceholder = 'red';
   }
   let basicPiece = {
     pieceX: x * cellSize + cellSize/2,
     pieceY: y * cellSize + cellSize/2,
     pieceD: cellSize/1.5,
     pieceGoal: GRID_SIZE - pieceGoalFinder,
-    isKing: false,
-    colour: colorPlaceholder,
   };
-
+    
   grid.push(basicPiece);
+  circle(basicPiece.pieceX, basicPiece.pieceY, basicPiece.pieceD);
 }
-
-function generateBoardArray(cols, rows) {
-  let newBoardGrid = [];
+    
+function generateBoard(cols, rows) {
+  let newGrid = [];
   for (let y = 0; y < rows; y++) {
-    newBoardGrid.push([]);
+    newGrid.push([]);
     tileIsLight = !tileIsLight;
     for (let x = 0; x < cols; x++) {
-      if (tileIsLight) {
-        newBoardGrid[y].push(LIGHT_TILE);
+      if (tileIsLight && y > 4) {
+        newGrid[y].push(playerPiece);
+      }
+      else if (tileIsLight && y < 3) {
+        newGrid[y].push(botPiece);
+      }
+      else if (tileIsLight) {
+        newGrid[y].push(LIGHT_TILE);
       }
       else {
-        newBoardGrid[y].push(DARK_TILE);
+        newGrid[y].push(DARK_TILE);
       }
       tileIsLight = !tileIsLight;
     }
   }
-  return newBoardGrid;
+  return newGrid;
 }
-
-function generatePiecesArray(cols, rows) {
-  let newPieceGrid = [];
-  for (let y = 0; y < rows; y++) {
-    newPieceGrid.push([]);
-    shouldBePiece = !shouldBePiece;
-    for (let x = 0; x < cols; x++) {
-      if (shouldBePiece && y < 3) {
-        newPieceGrid[y].push(botPiece);
-      }
-      else if (shouldBePiece && y > 4) {
-        newPieceGrid[y].push(playerPiece);
-      }
-    }
-  }
-}
-
-// Display pieces
+    
+    
 // Create a way to know what piece is selected
-// Make a way for piece to move
 // Make sure when piece is moved tile stays same colour; Likely white
+// Make a way for piece to mave
 // Make a way to capture
-// Create smart Ai (Make a func to check for possible capture)
+// Create smart Ai (Make a func to check for possible capture)ieceY, basicPiece.pieceD);
